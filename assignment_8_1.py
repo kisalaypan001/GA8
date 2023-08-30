759fabb1c05c6e5e0c231d03b8d487629a1e94a7
kisalaypan001 <143622068+kisalaypan001@users.noreply.github.com>
2023-08-30 20:26:46 +0530

    Update assignment_8_1.py

--- assignment_8_1.py
+++ assignment_8_1.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyOYH4nv+XKQFNnSptjoYTCj",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
